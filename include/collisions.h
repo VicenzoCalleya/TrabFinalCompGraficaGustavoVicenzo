60a683204e2094eee6f22326cfc979925b971d35
VicenzoCalleya <vicenzo.calleya@gmail.com>
2026-05-18 11:05:32 -0300

    Preenchendo collisions.h

--- include/collisions.h
+++ include/collisions.h
@@ -1,1 +1,9 @@
+#ifndef _COLLISIONS_H
+#define _COLLISIONS_H
 
+#include <glm/vec3.hpp>
+
+// Retorna true se houver colisão entre duas caixas AABB
+bool CheckCollision_AABB(glm::vec3 minA, glm::vec3 maxA, glm::vec3 minB, glm::vec3 maxB);
+
+#endif // _COLLISIONS_H
