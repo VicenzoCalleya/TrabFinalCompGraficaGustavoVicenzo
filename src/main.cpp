ebb1348969a7fbef7a054fd506754f2877727620
VicenzoCalleya <vicenzo.calleya@gmail.com>
2026-05-18 11:53:25 -0300

    Consertando erro de definições

--- src/main.cpp
+++ src/main.cpp
@@ -344,6 +344,13 @@ int main(int argc, char* argv[])
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
 
+    // Definindo variáveis
+    glm::mat4 model = Matrix_Identity();
+    glm::vec4 camera_position_c  = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 camera_lookat_l    = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 camera_up_vector   = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+    glm::vec4 camera_view_vector = glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+
     // Ficamos em um loop infinito, renderizando, até que o usuário feche a janela
     while (!glfwWindowShouldClose(window))
     {
