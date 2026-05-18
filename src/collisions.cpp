0861c6f92cc1e11e35172dafeb9d2d5106643805
VicenzoCalleya <vicenzo.calleya@gmail.com>
2026-05-18 11:10:14 -0300

    Criando função CheckCollisions

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -1,1 +1,16 @@
+#include "collisions.h"
 
+bool CheckCollision_AABB(glm::vec3 minA, glm::vec3 maxA, glm::vec3 minB, glm::vec3 maxB)
+{
+    // Verifica se há sobreposição no eixo X
+    bool collisionX = maxA.x >= minB.x && minA.x <= maxB.x;
+    
+    // Verifica se há sobreposição no eixo Y
+    bool collisionY = maxA.y >= minB.y && minA.y <= maxB.y;
+    
+    // Verifica se há sobreposição no eixo Z
+    bool collisionZ = maxA.z >= minB.z && minA.z <= maxB.z;
+    
+    // Há colisão apenas se houver sobreposição nos 3 eixos simultaneamente
+    return collisionX && collisionY && collisionZ;
+}
